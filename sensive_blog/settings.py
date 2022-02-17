930259c23e010b32a3f51060f717fa8148461799
c_Door in <egor1@mail.ru>
2022-02-17 23:56:32 +0300

    Setup debug tollbar

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -22,6 +22,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'blog',
+    'debug_toolbar',
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 MIDDLEWARE = [
@@ -32,6 +37,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'sensive_blog.urls'
