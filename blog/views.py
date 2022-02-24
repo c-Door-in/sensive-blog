1d2d2fd3ee1eb2ce0015e8f353bebeec353c3ad0
c_Door in <egor1@mail.ru>
2022-02-25 02:37:50 +0300

    Optimize comments count evaluation.

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,29 @@ from blog.models import Comment, Post, Tag
 
 
 def get_most_popular_posts():
-    posts = Post.objects.annotate(Count('likes')).prefetch_related('author')
-    popular_posts = posts.order_by('-likes__count')
-    return popular_posts[:5]
+
+    posts = Post.objects.annotate(likes_count=Count('likes')).prefetch_related('author')
+    popular_posts = posts.order_by('-likes_count')
+    most_popular_posts = popular_posts[:5]
+    popular_posts_ids = [post.id for post in most_popular_posts]
+    post_with_comments = Post.objects.filter(id__in=popular_posts_ids).annotate(comments_count=Count('comments'))
+    ids_and_comments = post_with_comments.values_list('id', 'comments_count')
+    count_for_id = dict(ids_and_comments)
+    for post in most_popular_posts:
+        post.comments_count = count_for_id[post.id]
+    
+    return most_popular_posts
+
+
+def get_most_fresh_posts():
+    fresh_posts = Post.objects.order_by(
+        '-published_at',
+    ).annotate(
+        comments_count=Count('comments'),
+    ).prefetch_related(
+        'author',
+    )
+    return list(fresh_posts)[:5]
 
 
 def get_most_popular_tags():
@@ -29,6 +49,20 @@ def serialize_post(post):
     }
 
 
+def serialize_post_optimized(post):
+    return {
+        'title': post.title,
+        'teaser_text': post.text[:200],
+        'author': post.author.username,
+        'comments_amount': post.comments_count,
+        'image_url': post.image.url if post.image else None,
+        'published_at': post.published_at,
+        'slug': post.slug,
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'first_tag_title': post.tags.all()[0].title,
+    }
+
+
 def serialize_tag(tag):
     return {
         'title': tag.title,
@@ -40,16 +74,15 @@ def index(request):
 
     most_popular_posts = get_most_popular_posts()
 
-    fresh_posts = Post.objects.order_by('-published_at').prefetch_related('author')
-    most_fresh_posts = list(fresh_posts)[:5]
+    most_fresh_posts = get_most_fresh_posts()
 
     most_popular_tags = get_most_popular_tags()
 
     context = {
         'most_popular_posts': [
-            serialize_post(post) for post in most_popular_posts
+            serialize_post_optimized(post) for post in most_popular_posts
         ],
-        'page_posts': [serialize_post(post) for post in most_fresh_posts],
+        'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
     return render(request, 'index.html', context)
